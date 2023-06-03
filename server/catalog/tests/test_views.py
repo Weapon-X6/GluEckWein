442a68a4f871389f941173b91287b330978943e4
waffe_x20 <argueta_eduardo@yahoo.de>
2023-06-02 19:07:13 -0600

    Add tests for query param

--- server/catalog/tests/test_views.py
+++ server/catalog/tests/test_views.py
@@ -14,3 +14,27 @@ class ViewTests(APITestCase):
         response = self.client.get('/api/v1/catalog/wines/')
         wines = Wine.objects.all()
         self.assertEqual(response.data, WineSerializer(wines, many=True).data)
+
+    def test_query_matches_variety(self):
+        response = self.client.get('/api/v1/catalog/wines/', {
+            'query': 'Cabernet',
+        })
+        self.assertEqual(1, len(response.data))
+        self.assertEqual("58ba903f-85ff-45c2-9bac-6d0732544841", response.data[0]['id'])
+
+    def test_query_matches_winery(self):
+        response = self.client.get('/api/v1/catalog/wines/', {
+            'query': 'Barnard',
+        })
+        self.assertEqual(1,len(response.data))
+        self.assertEqual("21e40285-cec8-417c-9a26-4f6748b7fa3a", response.data[0]['id'])
+
+    def test_query_matches_description(self):
+        response = self.client.get('/api/v1/catalog/wines/', {
+            'query': 'wine',
+        })
+        self.assertEqual(2, len(response.data))
+        self.assertCountEqual([
+            "58ba903f-85ff-45c2-9bac-6d0732544841",
+            "21e40285-cec8-417c-9a26-4f6748b7fa3a",
+        ], [item['id'] for item in response.data])
