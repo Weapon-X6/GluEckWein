a2bacd48d5792fd163b7644ab8db5d2f68ce418c
waffe_x20 <argueta_eduardo@yahoo.de>
2023-07-27 16:44:31 -0600

    Add fixture to e2e on client

--- client/cypress/e2e/all.cy.js
+++ client/cypress/e2e/all.cy.js
@@ -1,10 +1,11 @@
-describe('Persuable', () => {
-  it('Displays the home page.', () => {
+describe('Perusable.', () => {
+  it('Displays a list of results.', () => {
     cy.intercept('GET', '**/api/v1/catalog/wines/**', { fixture: 'wines.json' }).as('getWines');
 
     cy.visit('/');
-    cy.get('input#query').type('cabernet');
+    cy.get('input#query').type('staglin');
     cy.get('button').contains('Search').click();
+    cy.wait('@getWines');
     cy.get('div.card-title').should('contain', 'Cabernet Sauvignon');
   });
 });
