47bfb52911a2f56ae9b2a1100179657ed0683262
waffe_x20 <argueta_eduardo@yahoo.de>
2023-08-31 22:30:02 -0600

    Add pagination to Elasticsearch API

--- server/catalog/views.py
+++ server/catalog/views.py
@@ -1,3 +1,5 @@
+from urllib.parse import SplitResult, urlencode, urlsplit
+
 from elasticsearch_dsl import Search
 from elasticsearch_dsl.query import Match
 from rest_framework.generics import ListAPIView
@@ -26,21 +28,62 @@ class WineSearchWordsView(ListAPIView):
 
 
 class ESWinesView(APIView):
+    def _build_url(self, params, new_offset):
+        url = self.request.build_absolute_uri()
+        old_result = urlsplit(url)
+        new_result = SplitResult(
+            old_result.scheme,
+            old_result.netloc,
+            old_result.path,
+            query=urlencode({
+                **params,
+                'offset': new_offset,
+            }, doseq=True),
+            fragment='',
+        )
+        return new_result.geturl()
+
+    def _get_previous_page(self, params):
+        limit, offset = params.get('limit'), params.get('offset')
+        if offset > 0:
+            return self._build_url(params, offset - limit)
+        else:
+            return None
+
+    def _get_next_page(self, params, count):
+        limit, offset = params.get('limit'), params.get('offset')
+        if (offset + limit) < count:
+            return self._build_url(params, offset + limit)
+        else:
+            return None
+
     def get(self, request, *args, **kwargs):
         query = self.request.query_params.get('query')
+        limit = int(self.request.query_params.get('limit', 10))
+        offset = int(self.request.query_params.get('offset', 0))
 
         # Build Elasticsearch query.
         search = Search(index=constants.ES_INDEX)
-        response = search.query('bool', should=[
+        search = search.query('bool', should=[
             Match(variety=query),
             Match(winery=query),
             Match(description=query)
-        ]).params(size=100).execute()
+        ])[offset : offset + limit]
+
+        response = search.execute()
 
         return Response(data={
             'count': response.hits.total.value,
-            'next': None,
-            'previous': None,
+            'next': self._get_next_page({
+                'limit': limit,
+                'offset': offset,
+                'query': query,
+            }, count=response.hits.total.value),
+            'previous': self._get_previous_page({
+                'limit': limit,
+                'offset': offset,
+                'query': query,
+            }),
             'results': [{
                 'id': hit.meta.id,
                 'country': hit.country,
