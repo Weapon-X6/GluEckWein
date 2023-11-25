9a6a870578242f3801ac7dcf88411b09f8030dc6
waffe_x20 <argueta_eduardo@yahoo.de>
2023-11-24 23:12:01 -0600

    Add small improvements and update README

--- server/catalog/management/commands/elasticsearch.py
+++ server/catalog/management/commands/elasticsearch.py
@@ -8,25 +8,25 @@ from catalog.models import Wine
 
 
 class Command(BaseCommand):
-    help = 'Updates the Elasticsearch index.'
+    help = "Updates the Elasticsearch index."
 
     def _document_generator(self):
         for wine in Wine.objects.iterator():
             yield {
-                '_index': ES_INDEX,
-                '_id': wine.id,
-                'variety': wine.variety,
-                'country': wine.country,
-                'price': wine.price,
-                'winery': wine.winery,
-                'description': wine.description,
-                'points': wine.points,
+                "_index": ES_INDEX,
+                "_id": wine.id,
+                "variety": wine.variety,
+                "country": wine.country,
+                "price": wine.price,
+                "winery": wine.winery,
+                "description": wine.description,
+                "points": wine.points,
             }
 
     def handle(self, *args, **kwargs):
         connection = connections.get_connection()
-
         self.stdout.write(f'Checking if index "{ES_INDEX}" exists...')
+
         if connection.indices.exists(index=ES_INDEX):
             self.stdout.write(f'Index "{ES_INDEX}" already exists')
             self.stdout.write(f'Updating mapping on "{ES_INDEX}" index...')
@@ -35,15 +35,20 @@ class Command(BaseCommand):
         else:
             self.stdout.write(f'Index "{ES_INDEX}" does not exist')
             self.stdout.write(f'Creating index "{ES_INDEX}"...')
-            connection.indices.create(index=ES_INDEX, body={
-                'settings': {
-                    'number_of_shards': 1,
-                    'number_of_replicas': 0,
+            connection.indices.create(
+                index=ES_INDEX,
+                body={
+                    "settings": {
+                        "number_of_shards": 1,
+                        "number_of_replicas": 0,
+                    },
+                    "mappings": ES_MAPPING,
                 },
-                'mappings': ES_MAPPING,
-            })
+            )
             self.stdout.write(f'Index "{ES_INDEX}" created successfully')
 
         self.stdout.write(f'Bulk updating documents on "{ES_INDEX}" index...')
-        succeeded, _ = bulk(connection, actions=self._document_generator(), stats_only=True)
+        succeeded, _ = bulk(
+            connection, actions=self._document_generator(), stats_only=True
+        )
         self.stdout.write(f'Updated {succeeded} documents on "{ES_INDEX}" successfully')
